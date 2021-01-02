ee8cfb69bb37de1fb7a9875ee622655b6d9e5784
Afzaal B <afzaalopera@gmail.com>
2021-01-02 22:54:29 +0500

    PW Fix

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const app = express();
 // make sure to replace my db string & creds with your own
 mongoose
   .connect(
-    "mongodb+srv://gqlUser:99lahore99@cluster0.fwhil.mongodb.net/gqlDb?retryWrites=true&w=majority",
+    "mongodb+srv://gqlUser:E0pLVK1CQms713CJ@cluster0.fwhil.mongodb.net/gqlDb?retryWrites=true&w=majority",
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => {
